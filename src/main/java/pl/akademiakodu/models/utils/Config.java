6b1c146f4c1c5a8cb10afa79503716cbbfeb8f09
zientek <33483866+Zientek@users.noreply.github.com>
2017-11-24 14:59:58 +0100

    Delete Config.java

--- src/main/java/pl/akademiakodu/models/utils/Config.java
+++
@@ -1,9 +0,0 @@
-package pl.akademiakodu.models.utils;
-
-public class Config {
-    public static final String MYSQL_LINK = "jdbc:mysql://5.135.218.27:3306/Zen?characterEncoding=utf8";
-    public static final String MYSQL_LOGIN = "Zen";
-    public static final String MYSQL_PASSWORD = "zientek";
-    public static final String APP_ID = "4040b72adbcc730a03a11f3c7d560109";
-    public static final String APP_URL = "http://api.openweathermap.org/data/2.5/weather?q=";
-}
